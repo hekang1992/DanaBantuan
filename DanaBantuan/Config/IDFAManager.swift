fed8d82c0e484447bc4c6ac7698cc9b30e5a2a2e
hekang1992 <hekang19921111@hotmail.com>
2025-12-25 11:18:42 +0800

    IDFAManager

---
+++ DanaBantuan/Config/IDFAManager.swift
@@ -0,0 +1,64 @@
+//
+//  IDFAManager.swift
+//  DanaBantuan
+//
+//  Created by hekang on 2025/12/25.
+//
+
+import UIKit
+import AppTrackingTransparency
+import AdSupport
+
+class IDFAManager {
+    
+    static let shared = IDFAManager()
+    private init() {}
+    
+    func requestIDFA(completion: @escaping (String?) -> Void) {
+        let status = ATTrackingManager.trackingAuthorizationStatus
+        
+        switch status {
+        case .notDetermined:
+            ATTrackingManager.requestTrackingAuthorization { authStatus in
+                DispatchQueue.main.async {
+                    if authStatus == .authorized {
+                        let idfa = self.getCurrentIDFA()
+                        completion(idfa)
+                    } else {
+                        completion(nil)
+                    }
+                }
+            }
+            
+        case .authorized:
+            let idfa = getCurrentIDFA()
+            DispatchQueue.main.async {
+                completion(idfa)
+            }
+            
+        case .denied, .restricted:
+            DispatchQueue.main.async {
+                completion(nil)
+            }
+            
+        @unknown default:
+            DispatchQueue.main.async {
+                completion(nil)
+            }
+        }
+    }
+    
+    private func getCurrentIDFA() -> String {
+        guard ATTrackingManager.trackingAuthorizationStatus == .authorized else {
+            return ""
+        }
+        
+        let idfa = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+        
+        if idfa == "00000000-0000-0000-0000-000000000000" {
+            return ""
+        }
+        
+        return idfa
+    }
+}
--- DanaBantuan/Config/IDFVManager.swift
+++ DanaBantuan/Config/IDFVManager.swift
@@ -5,7 +5,6 @@
 //  Created by hekang on 2025/12/25.
 //
 
-
 import UIKit
 import Security
 
