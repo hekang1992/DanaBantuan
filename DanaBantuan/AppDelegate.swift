524e2973e80a572118c3f35d278aaf8d326421a1
hekang1992 <hekang19921111@hotmail.com>
2025-12-25 10:02:43 +0800

    Initial commit

---
+++ DanaBantuan/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  DanaBantuan
+//
+//  Created by hekang on 2025/12/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+    
+}
+
