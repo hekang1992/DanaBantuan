d18da7559888846555d925b5fcb9d7f7992355e8
hekang1992 <hekang19921111@hotmail.com>
2025-12-25 11:49:23 +0800

    LanguageManager

--- DanaBantuan/Config/LanguageManager.swift
+++ DanaBantuan/Config/LanguageManager.swift
@@ -5,33 +5,33 @@
 //  Created by hekang on 2025/12/25.
 //
 
-
 import Foundation
 
 enum AppLanguage: String {
-    case english = "462"
-    case indonesian = "460"
+    case en = "1"
+    case id = "2"
     
     var localeIdentifier: String {
         switch self {
-        case .english: return "en"
-        case .indonesian: return "id"
+        case .en: return "en"
+        case .id: return "id"
         }
     }
 }
 
 class LanguageManager {
     static var bundle: Bundle = .main
+    private static let userDefaultsKey = "app_language"
     
     static func setLanguage(code: Int) {
         let language: AppLanguage
         switch code {
         case 1:
-            language = .english
+            language = .en
         case 2:
-            language = .indonesian
+            language = .id
         default:
-            language = .english
+            language = .en
         }
         setLanguage(language)
     }
@@ -43,9 +43,17 @@ class LanguageManager {
         } else {
             bundle = .main
         }
+        UserDefaults.standard.set(language.rawValue, forKey: userDefaultsKey)
     }
     
     static func localizedString(for key: String) -> String {
         return bundle.localizedString(forKey: key, value: nil, table: nil)
+    }
+    
+    static var currentLanguage: AppLanguage {
+        guard let savedCode = UserDefaults.standard.string(forKey: userDefaultsKey) else {
+            return .en
+        }
+        return AppLanguage(rawValue: savedCode) ?? .en
     }
 }
